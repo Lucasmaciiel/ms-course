63c4744fc718f76f4ba4e75cf18a1a09dc9764b5
Lucas Maciel <lucasmacielgois@gmail.com>
2021-05-02 21:42:57 -0300

    Correção

--- hr-worker/src/main/java/com/lmg/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/lmg/hrworker/resources/WorkerResources.java
@@ -4,15 +4,12 @@ import com.lmg.hrworker.entities.Worker;
 import com.lmg.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/workes")
+@RequestMapping(value = "/workers")
 public class WorkerResources {
 
     @Autowired
--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,2 +1,3 @@
-Insert into tb_worker (name, daily_income) values ("Bob", 200.0);
-insert into tb_worker (name, daily_income) values ("Lucas", 300.00);
+insert into hr_worker (name, daily_income) values ('Bob', 200.0);
+insert into hr_worker (name, daily_income) values ('Lucas', 300.0);
+insert into hr_worker (name, daily_income) values ('Maria', 400.0);
