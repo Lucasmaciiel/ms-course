9f42d9dbe7cdf883e89bd09596f056a3b0ea030a
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-05-04 20:50:25 -0300

    Histrix fault tolerance

--- hr-payroll/src/main/java/com/lmg/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/lmg/hrpayroll/resources/PaymentResource.java
@@ -1,13 +1,15 @@
 package com.lmg.hrpayroll.resources;
 
-import com.lmg.hrpayroll.entities.Payment;
-import com.lmg.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lmg.hrpayroll.entities.Payment;
+import com.lmg.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -16,10 +18,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
 
+    }
+    
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+    	Payment payment = new Payment("Bran", 850.0, days);
+    	return ResponseEntity.ok(payment);
     }
 }
--- hr-worker/src/main/java/com/lmg/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/lmg/hrworker/resources/WorkerResources.java
@@ -35,6 +35,13 @@ public class WorkerResources {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
     	
+    /*	try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} */
+    	
     	logger.info("PORT = " + environment.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
